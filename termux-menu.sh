9ddf156096d42d93c7c7580a17da538bc24629fd
w1r4 <wirayudhia@gmail.com>
2019-11-25 14:08:47 +0700

    Update termux-menu.sh

--- termux-menu.sh
+++ termux-menu.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 # Bash Menu Script Example
+#  https://raw.githubusercontent.com/w1r4/termux-menu/master/termux-menu.sh
 
 PS3='Please enter your choice: '
 options=("Init" \
-         "Backup" \ 
-		 "Restore" \ 
-         "Enable Storage" \ 
-		 "Install VNC" \
-		 "Install Metasploit" \
-		 "install apkmod" \
-		 "Install MPS-YOUTUBE" \
-		 "Quit") \
-		 \
+         "Backup" \
+         "Restore" \
+         "Enable Storage" \
+         "Install VNC" \
+         "Install Metasploit" \
+         "install apkmod" \
+         "Install MPS-YOUTUBE" \
+         "Quit") 
 select opt in "${options[@]}"
 do
     case $opt in
-	    "Init")
-		    apt update && apt upgrade
-			apt install wget git nano mc aria2 -y
-			;;
+        "Init")
+            apt update && apt upgrade
+            apt install wget git nano mc aria2 -y
+            ;;
         "Backup")
-		    cd /data/data/com.termux/files
-			tar -cvzf /sdcard/termux-backup.tgz --owner=0 --group=0 home usr
-			;;
+            cd /data/data/com.termux/files
+            tar -cvzf /sdcard/termux-backup.tgz --owner=0 --group=0 home usr
+            ;;
         "Restore")
-			cd /data/data/com.termux/files
-			tar -xvzf /sdcard/termux-backup.tgz 
-			;;				
-	    "Enable Storage")
-		    termux-setup-storage
-		    ;;
+            cd /data/data/com.termux/files
+            tar -xvzf /sdcard/termux-backup.tgz
+            ;;                
+        "Enable Storage")
+            termux-setup-storage
+            ;;
         "Install VNC")
             pkg install bash-completion x11-repo tigervnc fluxbox -y
-			export DISPLAY=":1"
-			vncserver
+            export DISPLAY=":1"
+            vncserver
             ;;
         "Install Metasploit")
             pkg install unstable-repo metasploit -y
             ;;
-		"install apkmod")
+        "install apkmod")
             cd $HOME
-			wget https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
-            ;;			
+            wget  https://raw.githubusercontent.com/w1r4/termux-menu/master/termux-apk.sh
+            sh termux-apk.sh
+            ;;            
         "Install MPS-YOUTUBE")
-		    cd $HOME
-			pkg install python -y
-			pip install youtube_dl
-			pip install mps_youtube
-			pip install mpv
-			mpsyt set show_video true, \
-			      set download_command aria2c \
-				  --stream-piece-selector=inorder \
-				  --enable-http-pipelining=true \
-				  --min-split-size=4m \
-				  --file-allocation=none \
-				  --max-connection-per-server=10 \
-				  --split=10 --dir=%d --out=%f %u ,\
-                  set ddir /storage/downloads/ ,
-				  exit \
-                  \				  
+            cd $HOME
+            pkg install python -y
+            pip install youtube_dl
+            pip install mps_youtube
+            pip install mpv
+            mpsyt set show_video true, \
+                  set download_command aria2c \
+                  --stream-piece-selector=inorder \
+                  --enable-http-pipelining=true \
+                  --min-split-size=4m \
+                  --file-allocation=none \
+                  --max-connection-per-server=10 \
+                  --split=10 --dir=%d --out=%f %u ,\
+                  set ddir /storage/downloads/ , \
+                  exit 
             ;;
         "Quit")
             break
