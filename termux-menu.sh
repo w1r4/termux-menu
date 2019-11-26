25a224b4c7cffbe190bc5b42efd453ff834181ab
w1r4 <wirayudhia@gmail.com>
2019-11-26 10:38:29 +0700

    Update termux-menu.sh

--- termux-menu.sh
+++ termux-menu.sh
@@ -21,10 +21,11 @@ do
             termux-wake-lock
             pkg install bash-completion 
             apt update && apt upgrade
-            apt install wget git nano mc aria2 -y
-            echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> .termux/termux.properties
+            apt install wget git nano mc aria2 -y           
             curl https://raw.githubusercontent.com/w1r4/termux-dotfiles/master/setup.sh -o setup.sh
             bash setup.sh
+             echo "extra-keys = [['ESC','/','-','HOME','UP','END','PGUP'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT','PGDN']]" >> ~/.termux/termux.properties
+            termux-reload-settings
             termux-wake-unlock
             ;;
         "Backup")
