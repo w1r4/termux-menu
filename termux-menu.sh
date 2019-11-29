65e74173eee55b0acc1a639686821e5f53a87943
w1r4 <wirayudhia@gmail.com>
2019-11-29 15:45:35 +0700

    Update termux-menu.sh

--- termux-menu.sh
+++ termux-menu.sh
@@ -123,7 +123,7 @@ do
             qemu-system-x86_64 -nographic \
 	                      -net user,hostfwd=tcp::10022-:22,hostfwd=tcp::10080-:80 \
 			      -drive file=virtual_drive,media=disk,if=virtio   \   
-	                      -m 512M \
+	                      -m 512m \
 			      -loadvm new \
 	                      -smp 3
             ;;
